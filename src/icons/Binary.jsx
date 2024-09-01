0a13f593c43fd095b0f55c1c6a44dfe2b5d5bebc
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-08-31 20:30:45 -0600

    Fixed certain attributes that had errors in the Binary icon

--- src/icons/Binary.jsx
+++ src/icons/Binary.jsx
@@ -12,9 +12,9 @@ export default function Binary({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <rect x="14" y="14" width="4" height="6" rx="2"></rect>
       <rect x="6" y="4" width="4" height="6" rx="2"></rect>
